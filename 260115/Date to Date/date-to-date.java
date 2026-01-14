bf44ed21f965a2dbd6ec068e1f4f462303fe5454
youngwoofox-coder <youngwoo.fox@gmail.com>
2026-01-15 00:21:21 +0900

    Title: Date to Date, Time: 134ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 260115/Date to Date/date-to-date.java
+++ 260115/Date to Date/date-to-date.java
@@ -11,8 +11,8 @@ public class Main {
         int[] num_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         
         int result = 0;
-
-        result += (num_of_days[m1] -d1);
+        if(m1!=m2){
+            result += (num_of_days[m1] -d1);}
         for(int i=m1+1; i<m2;i++){
             result += num_of_days[i];
         }
