1a4e18589d0d611e70ad8cb288a97f47dddd0588
youngwoofox-coder <youngwoo.fox@gmail.com>
2026-01-17 00:57:26 +0900

    Title: 요일 맞추기, Time: 128ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260117/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        // Please write your code here.
+
+        int[] months = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int md1 =0;
+        int md2 =0;
+        int result_num=0;
+        
+        for(int i=0;i<=12;i++){
+            if(i<m1){
+                md1 += months[i];
+            }
+            if(i<m2){
+                md2 += months[i];
+            }
+        }
+        md1 += d1;
+        md2 += d2;
+
+        if(md1>md2){
+             result_num = md1-md2;
+             result_num = 7 - result_num;
+        }else {
+            result_num = md2-md1;
+        }
+        //System.out.println(md1);
+        //System.out.println(md2);
+        //System.out.println(result_num);
+
+        switch(result_num = result_num%7){
+        case 0: 
+            System.out.print("Mon");
+            break;
+        case 1: 
+            System.out.print("Tue");
+            break;
+        case 2: 
+            System.out.print("Wed");
+            break;
+        case 3: 
+            System.out.print("Thu");
+            break;
+        case 4: 
+            System.out.print("Fri");
+            break;
+        case 5: 
+            System.out.print("Sat");
+            break;
+        case 6: 
+            System.out.print("Sun");
+            break;
+        }
+
+    }
+}
