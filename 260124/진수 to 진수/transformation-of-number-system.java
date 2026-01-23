4b5321dc76d3705492b0dd8fae3caad838329248
youngwoofox-coder <youngwoo.fox@gmail.com>
2026-01-24 01:11:41 +0900

    Title: 진수 to 진수, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260124/진수 to 진수/transformation-of-number-system.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        String N = sc.next();
+        // Please write your code here.
+        int C =0;
+        int D =1;
+
+        for(int i= N.length() -1;i>=0;i--){
+            int N_num = N.charAt(i) -'0';
+            C = C + N_num * D;
+            D = D* A;
+        }
+        //System.out.print(C);
+
+        int B_share =0;
+        int B_remian=0;
+        String result = "";
+        while(C>=1){
+            B_remian = C%B;
+            C = C/B;
+            result = result + B_remian;
+        }
+        System.out.print(result);
+
+    }
+}
