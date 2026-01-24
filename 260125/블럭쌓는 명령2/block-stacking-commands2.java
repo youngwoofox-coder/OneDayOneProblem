81a0bd15cf966dee21247bf1028c6f109abdea0a
youngwoofox-coder <youngwoo.fox@gmail.com>
2026-01-25 01:42:13 +0900

    Title: 블럭쌓는 명령2, Time: 134ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260125/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] array = new int[N];
+        for (int i = 0; i < K; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+
+            for(int j=0;j<N;j++){
+                if(A<=j && j<=B){
+                    array[j-1] = array[j-1] + 1;
+                    //System.out.println(array[j]);
+                }
+            }
+
+        }
+        // Please write your code here.
+        int result =array[0];
+        for (int i = 0; i < N-1 ; i++){
+            if(array[i]<array[i+1]){
+                result = array[i+1];
+            }
+        }
+        
+
+       System.out.print(result);
+    }
+}
