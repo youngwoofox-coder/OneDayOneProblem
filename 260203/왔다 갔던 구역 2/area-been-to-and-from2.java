c46dc5f98ea840ae718a88e10b0c617d4f5accc6
youngwoofox-coder <youngwoo.fox@gmail.com>
2026-02-03 01:00:43 +0900

    Title: 왔다 갔던 구역 2, Time: 136ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260203/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int point =500;
+        int[] array = new int[1000];
+        int min=500;
+        int max=500;
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            char y = sc.next().charAt(0);
+            int zero=0;
+            while(zero<x){
+                zero++;
+                if(y=='R'){array[point]++; point++;}
+                else {array[point]++; point--;}
+
+                if(point<min){min=point;}
+                if(max<point){max=point;}
+            }
+        }
+        // Please write your code here.
+
+        int result =0;
+        //System.out.println(min);
+        //System.out.println(max);
+        for(int i=min; i<=max;i++){
+            if(array[i]>=2){result++;}
+            //System.out.println(array[i]);
+        }
+
+        System.out.print(result);
+    }
+}
