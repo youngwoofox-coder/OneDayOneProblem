ba7f2ef7123950c3c1f540730085cf7524c62d86
youngwoofox-coder <youngwoo.fox@gmail.com>
2026-03-11 01:12:01 +0900

    Title: 선택 정렬 구현, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260311/선택 정렬 구현/implement-selection-sort.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int temp =0;
+        
+        for(int i=0;i<n;i++){
+            int min =i;
+            for(int j=i;j<n;j++){
+                if(arr[j]<arr[min]){
+                    min=j;
+                }
+            }
+            temp=arr[i];
+            arr[i]=arr[min];
+            arr[min]=temp;
+        }
+
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+        // Please write your code here.
+    }
+}
